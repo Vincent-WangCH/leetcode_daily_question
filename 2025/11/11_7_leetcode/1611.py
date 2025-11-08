e198a0c56fe97911d26ace4fcab6a33c66322e39
Vincent-WangCH <74845578+Vincent-WangCH@users.noreply.github.com>
2025-11-07 22:11:22 -0800

    11-7-2025 leetcode

---
+++ 2025/11/11_7_leetcode/1611.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+
+        if n == 0:
+            return 0
+        k = n.bit_length()-1
+        top = 1 << k
+        return (1 << (k+1)) - 1 - self.minimumOneBitOperations(n ^ top)
